526c14e4da547c661bbbc2792831663d7caefc1c
Nereo Ontiveros <nerontiveros@gmail.com>
2020-08-09 20:01:22 +0100

    feat: scrolling through all the movies

--- app/src/main/java/com/nereoontiveros/popularmovies2/view/MainActivity.kt
+++ app/src/main/java/com/nereoontiveros/popularmovies2/view/MainActivity.kt
@@ -4,19 +4,76 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.nereoontiveros.popularmovies2.R
 import com.nereoontiveros.popularmovies2.model.Movie
 import com.nereoontiveros.popularmovies2.utils.MoviesRepository
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var popularMovies: RecyclerView
+    private lateinit var popularMoviesAdapter: MoviesAdapter
+    private lateinit var popularMoviesLayoutMgr: LinearLayoutManager
+
+    private var popularMoviesPage = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        popularMovies = findViewById(R.id.popular_movies)
+        popularMoviesLayoutMgr = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        popularMovies.layoutManager = popularMoviesLayoutMgr
+        popularMoviesAdapter = MoviesAdapter(mutableListOf())
+        popularMovies.adapter = popularMoviesAdapter
+        getPopularMovies()
+
         MoviesRepository.getPopularMovies(
-            onSuccess = {movies-> Log.d("MainActivity", "Movies: $movies")},
-            onError = {Toast.makeText(this, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()}
+            popularMoviesPage,
+             ::onPopularMoviesFetched,
+             ::onError
         )
     }
+
+    private fun onPopularMoviesFetched(movies: List<Movie>) {
+        popularMoviesAdapter.appendMovies(movies)
+        attachPopularMoviesOnScrollListener()
+    }
+    private fun onError(){
+        Toast.makeText(this, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()
+    }
+    private fun getPopularMovies() {
+        MoviesRepository.getPopularMovies(
+            popularMoviesPage,
+            ::onPopularMoviesFetched,
+            ::onError
+        )
+    }
+    private fun attachPopularMoviesOnScrollListener() {
+        popularMovies.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                //the total number of movies inside popularMoviesAdapter. will be encreasing
+                val totalItemCount = popularMoviesLayoutMgr.itemCount
+                //the current number of child views attached to the RecyclerView currently being recycled
+                val visibleItemCount = popularMoviesLayoutMgr.childCount
+                //the position fo the leftmost visible item in the list
+                val firstVisibleItem = popularMoviesLayoutMgr.findFirstVisibleItemPosition()
+
+                //the condition will be true if the user scrolls past halfway plus buffered value of visibleItemCount
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
+                    popularMovies.removeOnScrollListener(this)//disable scroll listener
+                    popularMoviesPage++
+                    getPopularMovies()
+                }
+            }
+        })
+    }
+
+
 
 }
---
+++ app/src/main/java/com/nereoontiveros/popularmovies2/view/MoviesAdapter.kt
@@ -0,0 +1,49 @@
+package com.nereoontiveros.popularmovies2.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.nereoontiveros.popularmovies2.R
+import com.nereoontiveros.popularmovies2.model.Movie
+
+class MoviesAdapter(
+    private var movies: MutableList<Movie>//dynamic list
+) : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    fun appendMovies(movies: List<Movie>) {
+        this.movies.addAll(movies)
+        notifyItemRangeInserted(//notify there are new items added from this start and end positions
+            this.movies.size,
+            movies.size-1
+        )
+    }
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val poster: ImageView = itemView.findViewById(R.id.item_movie_poster)
+
+        fun bind(movie: Movie) {
+            Glide.with(itemView)
+                .load("https://image.tmdb.org/t/p/w342${movie.posterPath}")
+                .transform(CenterCrop())
+                .into(poster)
+        }
+    }
+}
